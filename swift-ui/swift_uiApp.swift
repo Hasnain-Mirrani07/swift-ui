a807b65fd62d3db83cfe3961d8f03bf4909256c3
Hasnain-Mirrani07 <115531664+Hasnain-Mirrani07@users.noreply.github.com>
2023-08-14 15:31:12 +0500

    Initial Commit

---
+++ swift-ui/swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_uiApp.swift
+//  swift-ui
+//
+//  Created by mac on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
